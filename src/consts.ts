9f2e81ecda5b4449313732df6e06f6bf70ec6c51
vladi-kz <vladimir.hkz@gmail.com>
2024-05-30 10:00:34 -0400

    Cambia contenido del head

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kasi Zavio Blog';
-export const SITE_DESCRIPTION = 'Welcome to: mis divagaciones.';
+export const SITE_TITLE = 'Kasi Zavio | Blog';
+export const SITE_DESCRIPTION = 'Salud y entrenamiento, humor, tecnología, libertad, busca de sentido y muchas divagaciones.';
